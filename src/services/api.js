dd0c61380eaf90cdf9de89bc54d662d51fa4b475
qroulland <qroulland@gmail.com>
2021-11-07 18:16:08 +0100

    fix: api call in prod

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = process.env.NODE_ENV === 'production' ? 'https://damp-inlet-82314.herokuapp.com/api' : 'http://localhost:8000/api';
 
 async function getCats() {
   return await (await axios.get(`${API_URL}/cats`)).data.data;
